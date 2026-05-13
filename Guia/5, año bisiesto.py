386f672cc1c2f5a9e5243a68e8cc78421dc011df
dukerixx-dev <dukerixx@gmail.com>
2026-05-13 15:52:30 -0400

    xdxdxdxdxdxd

--- Guia/5, año bisiesto.py
+++ Guia/5, año bisiesto.py
@@ -1,11 +1,17 @@
-año=int(input("ingrese el año que quiere revisar si es bisiesto "))
+año=int(input("ingrese el año que quiere revisar si es bisiesto: "))
 multiplo_4 = año%4
 multiplo_100 = año%100
 
 #hacer un ciclo que evalue los multiplos de 4, 10 y de los dos juntos hasta que llegue al año que eligio el usario
 
-if multiplo_4 == 0 and multiplo_100 != 0:
-    print(f"el año {año} es bisiesto")
+if año%4 == 0:
+    if año%100 == 0:
+        if año%400 == 0:
+            print(f"el año {año} es bisiesto") 
+        else:
+            print(f"el año {año} No es bisiesto")
+    else:
+        print(f"el año {año} es bisiesto") 
 else:
     print(f"el año {año} No es bisiesto")
 
